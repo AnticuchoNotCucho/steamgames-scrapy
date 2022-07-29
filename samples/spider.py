aced2f24326684bde08d9a1d8e30e557fe4c6300
anticuchonotcucho <jorge_2jul@hotmail.com>
2022-07-29 04:00:10 -0400

    Datos almacenados parcialmente en mongodb, valores de cada cromo y sus respectivos juegos.

--- samples/spider.py
+++ samples/spider.py
@@ -5,8 +5,13 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.spiders import Spider
 
 
-# defino una clase que hereda de Spider
+def cleandata(text):
+    new_text = text.replace('[', '').replace(']', '').replace("'", '').replace('https://steamcommunity.com/market'
+                                                                               '/listings/753/', '')
+    return new_text
 
+
+# defino una clase que hereda de Spider
 class SearchCards(Spider):
     name = 'search_cards'
     # inicializo el mongo cliente
@@ -21,24 +26,28 @@ class SearchCards(Spider):
         'LOG_ENABLED': False  # Elimina los miles de logs que salen al ejecutar Scrapy en terminal
     }
 
-    # Generamos url en base a las appids de cada juego almacenado en el json
+    # Generamos url en base a las appids de cada juego almacenado en la base de datos
     def start_requests(self):
-        with open('games.json', 'r') as f:
-            data = json.load(f)
-            for game in data:
-                yield scrapy.Request(
-                    url='https://www.steamcardexchange.net/index.php?gamepage-appid-' + game['AppID'],
-                    callback=self.parse, meta={'game': game['AppID']})
+        games = self.db['games']
+        for game in games.find():
+            yield scrapy.Request(
+                url='https://www.steamcardexchange.net/index.php?gamepage-appid-' + game['AppID'],
+                callback=self.parse, meta={'game': game['AppID']})
 
+    # Parseamos la pagina de cada juego
     def parse(self, response, **kwargs):
         cards = response.xpath('//*[@id="content-area"]/div[2]/div[4]//d'
                                'iv[@class="showcase-element"]/div/a/@href').getall()
+        # por cada link de cromo, almacenamos su hash_name y appid
         for card in cards:
+            card = cleandata(card)
             game = response.meta['game']
             collection = self.db['cards']
-            collection.insert_one({'game_id': game, 'hash_name': card})
+            collection.insert_one({'_id': card, 'game_id': game, 'hash_name': card})
+        print('Cards added to database')
 
 
+# nos permite ejecutar el programa desde el main sin necesidad de ejecutar scrapy.py
 process = CrawlerProcess()
 process.crawl(SearchCards)
-process.start()  # the script will block here until the crawling is finished
+process.start()
--- samples/spider2.py
+++ samples/spider2.py
@@ -1,22 +1,60 @@
 import json
-
 import scrapy
+from pymongo import MongoClient
 from scrapy.crawler import CrawlerProcess
 from scrapy.spiders import Spider
 
 
 class SearchPrices(Spider):
     name = 'get_prices'
-    allowed_domains = ['steamcommunity.com', 'steamcardexchange.net']
+    # inicializo el mongo cliente
+    client = MongoClient('localhost', 27017)
+    db = client['profit']
+    list_games = []
+    allowed_domains = ['steamcommunity.com']
     custom_settings = {
         'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/84.0.4147.135 Safari/537.36',
-        'LOG_ENABLED': True
+        'LOG_ENABLED': True,
+        'LOG_LEVEL': 'INFO',
+        'CONCURRENT_REQUESTS': 1,
+        'DOWNLOAD_DELAY': 0.5,
     }
 
+    # generamos lista de juegos para iterar sobre ellos
+    # def generate_list(self):
+    #   games = self.db['games']
+    #   for game in games.find():
+    #       game_id = game['AppID']
+    #       game_title = game['Title']
+    #      self.list_games.append([game_id, game_title])
+
+    # comenzamos la iteracion en los links de los cromos
     def start_requests(self):
-        with open('appids.json', 'r') as f:
-            data = json.load(f)
+        games = self.db['games']
+        for game in games.find():
+            cards = self.db['cards']
+            card_list = cards.find({'game_id': game['AppID']})
+            for card in card_list:
+                yield scrapy.Request(
+                    url='https://steamcommunity.com/market/priceoverview/?appid=753&currency=34&market_hash_name=' +
+                        card['hash_name']
+                    , callback=self.parse, meta={'game': game, 'card': card})
+
+    # parseamos la respuesta de cada cromo y almacenamos los precios en la base de datos
+    def parse(self, response, **kwargs):
+        game = response.meta['game']
+        card = response.meta['card']
+        response_json = json.loads(response.body)
+        lowest_price = response_json['lowest_price']
+        median_price = response_json['median_price']
+        collection = self.db['cards_prices']
+        collection.insert_one({'hash_name': card['hash_name'],
+                               'game_title': game['Title'],
+                               'lowest_price': lowest_price,
+                               'median_price': median_price})
 
 
-
+process = CrawlerProcess()
+process.crawl(SearchPrices)
+process.start()
